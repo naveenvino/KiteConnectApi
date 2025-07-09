e80d613010a7e04bf4d385e828b8288af9c1371b
NaveenVino <d.naveenkumar8@gmail.com>
2025-07-09 14:34:13 +0530

    commit

---
+++ Migrations/20250709085935_InitialAzureSQLMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KiteConnectApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialAzureSQLMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Realised",
+                table: "TradePositions",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Unrealised",
+                table: "TradePositions",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Realised",
+                table: "TradePositions");
+
+            migrationBuilder.DropColumn(
+                name: "Unrealised",
+                table: "TradePositions");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -175,7 +175,11 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "KiteConnectApi v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
